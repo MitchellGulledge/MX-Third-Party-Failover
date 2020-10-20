a439c9c241a1ab5ed4f3be99e72a137b4a9ade5c
MitchellGulledge <61255163+MitchellGulledge@users.noreply.github.com>
2020-10-19 17:58:25 -0700

    Update failover.py

--- failover.py
+++ failover.py
@@ -1,61 +1,121 @@
 import requests, json, time
 import meraki
 import re
-import datetime as dt
-from datetime import datetime, timedelta
 
 # Meraki credentials are placed below
 meraki_config = {
 	'api_key': "",
-	'org_name': ""
+	'org_name': "",
+    'tag_placeholder_network': "tag-placeholder",
+    'tag_prefix': "vwan-"
 }
 
-# this function obtains the org ID by inputting org name and API key
-mdashboard = meraki.DashboardAPI(meraki_config['api_key'])
-result_org_id = mdashboard.organizations.getOrganizations()
-for x in result_org_id:
-    if x['name'] == meraki_config['org_name']:
-        meraki_config['org_id'] = x['id']
+# Function to authenticate against Meraki SDK with the API key
+def get_api_auth():
+    meraki_config['api_auth'] = meraki.DashboardAPI(
+        meraki_config['api_key']
+        )
 
-# this function performs an org wide Meraki call for all sites VPN statuses
-# not using the SDK for this call as it is currently unavailable for now..
-def org_wide_vpn_status(list_of_meraki_ids):
-    # defining the URL for the GET below
-    org_vpn_url = 'https://api.meraki.com/api/v1/organizations/'\
-        +meraki_config['org_id']+'/appliance/vpn/statuses?networkIds[]='\
-            +str(list_of_meraki_ids[0]) # this is not the correct way to do this, but leaving for now
-    # creating the header in order to authenticate the call
-    header = {"X-Cisco-Meraki-API-Key": meraki_config['api_key'], "Content-Type": "application/json"}
-    # performing API call to meraki dashboard
-    vpn_statuses = requests.get(org_vpn_url, headers=header).content
-    print(vpn_statuses)
+# Function to obtain organization ID
+def get_org_id():
+    # obtaining all orgs with corresponding API key
+    result_org_id = meraki_config['api_auth'].organizations.getOrganizations()
+    # iterating through list of orgs to match specified org_name
+    for orgs in result_org_id:
+        if orgs['name'] == meraki_config['org_name']:
+            meraki_config['org_id'] = orgs['id']
+
+# function to obtain a list of all vpn stats for a network
+def get_vpn_stats(list_of_networks):
+
+    get_vpn_stats.response = meraki_config['api_auth'].appliance.getOrganizationApplianceVpnStatuses(
+        meraki_config['org_id'], total_pages='all', networkIds=list_of_networks
+    )
 
 # this function iterates through a list of networks obtained via an org wide call
 def get_meraki_networks_by_tag():
+
     # creating a list of network IDs with specified tag
     get_meraki_networks_by_tag.list_of_network_ids = []
+
     # obtaining complete list of networks within org
-    meraki_network_list = mdashboard.networks.getOrganizationNetworks(meraki_config['org_id'])
+    meraki_network_list = meraki_config['api_auth'].organizations.getOrganizationNetworks(
+        meraki_config['org_id']
+        )
+
     # looping through list of networks with anything matching a specific tag
     for network in meraki_network_list:
         # filtering for networks tagged with vWAN- and skipping over network Tag-Placeholder
-        if network['tags'] and network['name'] != 'Tag-Placeholder' and "vWAN-" in network['tags']:
-            # obtaining network ID to later append to the list of network ids
-            network_id = network['id']
-            get_meraki_networks_by_tag.list_of_network_ids.append(network_id) # appending network ID to list
+        if network['tags'] and network['name'] != 'Tag-Placeholder' and \
+            str(meraki_config['tag_prefix']) in str(network['tags']):
+                # appending network ID to list
+                get_meraki_networks_by_tag.list_of_network_ids.append(str(network['id'])) 
+    
+    print(get_meraki_networks_by_tag.list_of_network_ids)
 
-# this function swaps network tags if a failover is detected
-def swap_network_tag(network_id):
-    # API call to obtain current tag on network
-    current_network_info = mdashboard.networks.getNetwork(network_id)
-    current_network_tag = current_network_info['tags'] # parsing network info for tags
-    print(current_network_tag)
+# this function swaps the network tag if the vpn peer is detected as down
+def swap_tag(network_id):
 
-# end of function list
+    # Performing GET to obtain all networks w/ tags in organization
+    meraki_network_info = meraki_config['api_auth'].organizations.getOrganizationNetworks(
+    meraki_config['org_id'], tagsFilterType = 'withAnyTags'
+    )
 
+    # iterating through networks to match our network ID
+    for meraki_networks in meraki_network_info:
+        if meraki_networks['id'] == str(network_id):
+            network_tags = meraki_networks['tags']
+            print(network_tags)
+
+            # regex to match the tags for primary and secondary tunnel availability tags
+            primary_tag_regex = f"(?i)^{meraki_config['tag_prefix']}([a-zA-Z0-9_-]+)-[0-9]+$"
+            secondary_tag_regex = f"(?i)^{meraki_config['tag_prefix']}([a-zA-Z0-9_-]+)-[0-9]+-sec$"
+
+            # Get specific vwan tag from the list
+            for tag in range(0, len(network_tags)):
+                if re.match(primary_tag_regex, network_tags[tag]):
+                    # creating new tag to append to the list
+                    specific_tag = str(network_tags[tag]) + "-sec"
+                    network_tags[tag] = specific_tag
+                elif re.match(secondary_tag_regex, network_tags[tag]):
+                    # removing -sec from the tag value
+                    specific_tag = str(network_tags[tag])[-4]
+                    network_tags[tag] = specific_tag
+
+            print(network_tags)
+
+            # updating Meraki networks with new tag list network_tags
+
+            update_net = meraki_config['api_auth'].networks.updateNetwork(
+                network_id, 
+                tags=network_tags
+            )
+
+            print(update_net)
+
+
+# Obtaining auth to the Meraki SDK (returning variable to later call when using SDK)
+get_api_auth()
+
+# executing function to obtain Organization ID
+get_org_id()
+
+# executing function to obtain list of networks by tag
 get_meraki_networks_by_tag()
-# below is the list of network IDs that are built with the get_meraki_networks_by_tag() function
-print(get_meraki_networks_by_tag.list_of_network_ids)
 
-org_wide_vpn_status(get_meraki_networks_by_tag.list_of_network_ids)
-swap_network_tag(get_meraki_networks_by_tag.list_of_network_ids[0])
+# executing function to get a complete list of VPN stats
+get_vpn_stats(get_meraki_networks_by_tag.list_of_network_ids)
+
+# filtering out all NoneType elements from list so we can iterate through
+new_list = [x for x in get_vpn_stats.response if x != None]
+
+# iterating through list of vpn stats obtained via get_vpn_stats function
+for networks in new_list:
+    if networks['thirdPartyVpnPeers'][0]['reachability'] == 'unreachable':
+        print("VPN peer detected as unreachable, swapping tags")
+
+        # executing function to swap tags for the network
+        swap_tag(networks['networkId'])
+
+        time.sleep(5)
+        
